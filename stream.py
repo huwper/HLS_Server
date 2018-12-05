e64e662fef466439cc88329111a499836be6be1e
Huw Percival <huw.percival@btinternet.com>
2018-12-05 19:17:53 +0000

    stream.py can lauch a ffmpeg process

---
+++ stream.py
@@ -0,0 +1,18 @@
+from subprocess import Popen, PIPE
+
+#ffmpeg stream command
+ffmpeg = ['ffmpeg', '-f', 'alsa', '-i', 'default:CARD=Device'\
+		, '-ac', '1', '-hls_time', '1', '-hls_list_size'\
+		, '10', '-hls_flags', 'delete_segments', './hls/ronastream.m3u8']
+
+#server command
+
+if __name__ == '__main__':
+	f = Popen(ffmpeg, stderr = PIPE, stdout = PIPE)
+	print('ffmpeg started')
+	
+	try:
+		f.wait()
+	except KeyboardInterrupt:
+		print('terminating process')
+		f.kill()
