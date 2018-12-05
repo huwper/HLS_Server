3b54741f8c616cf0829c735c818b9ef0c38b31db
Huw Percival <huw.percival@btinternet.com>
2018-12-05 21:44:13 +0000

    boosted volume and added a filter, also print to log file if ffmpeg crashes

--- stream.py
+++ stream.py
@@ -1,19 +1,28 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
+from datetime import datetime
 
 #ffmpeg stream command
 ffmpeg = ['ffmpeg', '-f', 'alsa', '-i', 'default:CARD=Device'\
-		, '-ac', '1', '-hls_time', '1', '-hls_list_size'\
-		, '5', '-hls_flags', 'delete_segments', './hls/ronastream.m3u8']
+		, '-ac', '1', '-af', 'highpass=f=200', '-af', 'lowpass=f=3000', '-af', 'volume=5'\
+		, '-hls_time', '1', '-hls_list_size', '5'\
+		, '-hls_flags', 'delete_segments', '/home/pi/repos/HLS_Server/hls/ronastream.m3u8']
 
 #server command
 
 if __name__ == '__main__':
-	f = Popen(ffmpeg, stderr = PIPE, stdout = PIPE)
-	print('ffmpeg started')
+	with open('/home/pi/repos/HLS_Server/log.txt', mode='a') as log:
+		log.write('\r\n\r\n>>>>>>>>>> started at {}\r\n'.format(str(datetime.now())))
+
+
+		f = Popen(ffmpeg, stderr = STDOUT, stdout = PIPE, universal_newlines = True)
 	
-	try:
-		f.wait()
-	except KeyboardInterrupt:
-		print('terminating process')
-		f.communicate("q")
-		f.wait()
+		try:
+			f.wait()
+			for line in f.stdout:
+				log.write(line)
+			log.write('\r\n\r\n>>>>>>>> process finished at {}\r\n'.format(str(datetime.now)))
+
+		except KeyboardInterrupt:
+			f.communicate("q")
+			f.wait()
+			log.write('\r\n\r\n>>>>>>>> process terminated at {}\r\n'.format(str(datetime.now())))
